74124e8d9e0ce71a9c28cfb801fb9b849f06f6d8
wt <huifer97@163.com>
2019-12-27 14:26:30 +0800

    123

---
+++ src/main/java/com/huifer/en/Rte.java
@@ -0,0 +1,9 @@
+package com.huifer.en;
+
+public class Rte {
+
+	public static boolean hh(int i) {
+		return i <= 0;
+	}
+
+}
---
+++ src/test/java/com/huifer/en/RteTest.java
@@ -0,0 +1,12 @@
+package com.huifer.en;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RteTest {
+
+	@Test
+	public void te() {
+		Assert.assertEquals(true, Rte.hh(1));
+	}
+}
