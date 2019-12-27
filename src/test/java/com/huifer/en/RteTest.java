d3917e3430caa4a361fb0ae9c88a7213d603b102
wt <huifer97@163.com>
2019-12-27 16:25:16 +0800

    cob7

--- src/test/java/com/huifer/en/RteTest.java
+++ src/test/java/com/huifer/en/RteTest.java
@@ -7,16 +7,16 @@ public class RteTest {
 
 	@Test
 	public void te() {
-		Assert.assertEquals(false, Rte.hh(1));
+		Assert.assertFalse(Rte.hh(1));
 	}
 
 	@Test
 	public void te2() {
-		Assert.assertEquals(true, Rte.hh(0));
+		Assert.assertTrue(Rte.hh(0));
 	}
 
-	@Test
-	public void error() {
-		int i = 1 / 0;
-	}
+//	@Test
+//	public void error() {
+//		int i = 1 / 0;
+//	}
 }
