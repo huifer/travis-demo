94c2fda257a3879aba1842c74cf5d4451fac53a9
huifer <huifer97@163.com>
2019-12-27 18:51:31 +0800

    rc2

--- src/test/java/com/huifer/en/RteTest.java
+++ src/test/java/com/huifer/en/RteTest.java
@@ -7,12 +7,12 @@ public class RteTest {
 
 	@Test
 	public void te() {
-		Assert.assertFalse(Rte.hh(1));
+		Assert.assertTrue(Rte.hh(1));
 	}
 
 	@Test
 	public void te2() {
-		Assert.assertTrue(Rte.hh(0));
+		Assert.assertFalse(Rte.hh(0));
 	}
 
 //	@Test
